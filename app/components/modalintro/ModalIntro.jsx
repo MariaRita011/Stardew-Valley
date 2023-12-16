45c9443c37de30811b2ae1d85376b947e15605c5
Maria Rita Gomes <mariagomes@aluno.senai.br>
2023-12-16 20:49:51 -0300

    feat: modal

--- app/components/modalintro/ModalIntro.jsx
+++ app/components/modalintro/ModalIntro.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from "./modalIntro.module.css";
 
-const ModalIntro = ({ imagem, nome, texto }) => {
+const ModalIntro = ({ imagem, nome, texto, fechar }) => {
   const [aparecerTexto, setAparecerTexto] = useState('');
   const [index, setIndex] = useState(0);
 
@@ -14,7 +14,7 @@ const ModalIntro = ({ imagem, nome, texto }) => {
       } else {
         clearInterval(intervalId);
       }
-    }, 50); // ajuste a velocidade conforme necessário
+    }, 35); // ajuste a velocidade conforme necessário
 
     return () => clearInterval(intervalId);
   }, [texto, index]);
@@ -24,9 +24,14 @@ const ModalIntro = ({ imagem, nome, texto }) => {
       <div className={styles.textoIntro}>
         <span className={styles.text}>{aparecerTexto}</span>
       </div>
+      <div className={styles.infos}>
+        <div className={styles.containerButtonClose}>
+      <button onClick={fechar} className={styles.buttonClose} ><p className={styles.close}>x</p></button>
+        </div>
       <div className={styles.imgNome}>
         <img className={styles.imagem} src={imagem} width={200} height={200} alt="Imagem" />
         <h1 className={styles.titulo}>{nome}</h1>
+      </div>
       </div>
     </div>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,30 @@
+'use client'
 import styles from './page.module.css'
 import ModalIntro from './components/modalintro/ModalIntro'
+import { useState } from 'react'
 
 export default function Home() {
 
+  const [modal, setModal] = useState(true)
+
+  function fechar() {
+    setModal(false)
+  }
+
   return (
     <main className={styles.containerPrincipal}>
-      <ModalIntro
-        nome={"teste"}
+      {
+        modal ? (
+<ModalIntro
+        nome={"Maria Rita"}
         imagem={"/pngs/mrperson.png"}
+        fechar={fechar}
         texto={"Ola, seja bem-vindo(a) ao meu site de Stardew Valley. Aqui voce encontra um guia completo (ou quase completo) do jogo. Lembrando que esse e um site totalmente feito afim de estudos. Divirta-se!"} />
+        ) : (
+          null
+        )
+      }
+      
     </main>
   )
 }
